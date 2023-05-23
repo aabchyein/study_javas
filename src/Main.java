9fd94ce3b29c76caf51606fd10cf3b899eec02c0
aabchyein <abcqaz369@naver.com>
2023-05-23 17:26:27 +0900

    2023-05-23

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         String name = "HyeIn Kim";
         System.out.println(name);
+        name = "Kim";
+        System.out.println();
        // return 0;
     }
 }
---
+++ src/MakingGamebyOneHand.java
@@ -0,0 +1,20 @@
+public class MakingGamebyOneHand {
+    public static void main(String[] args) {
+        String A = "1";
+        String B = "";
+        String C = "2";
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+        
+        B = C;
+        C = A;
+        A = B;
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+
+        // return 0;
+    }
+}
+
