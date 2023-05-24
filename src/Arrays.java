a821224c81b32052afaf8fc6515d98b318fe08f3
aabchyein <abcqaz369@naver.com>
2023-05-24 18:44:09 +0900

    2023-05-24 five

---
+++ src/Arrays.java
@@ -0,0 +1,12 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        
+
+        for (int number=0; number < 4 ; number = number + 1) {
+            System.out.print(cars[number]+" ");
+        }
+        // return 0;
+    }
+    
+}
--- src/LoopNestedLoops.java
+++ src/LoopNestedLoops.java
@@ -3,7 +3,7 @@ public class LoopNestedLoops {
 
         for (int first = 1; first < 5; first = first + 1) {
             System.out.println("for " +  first);
-            for (int second=1; second  <=4; second = second + 1) {
+            for (int second=1; second  <= 4; second = second + 1) {
                 if (second <= first) {
                     System.out.print("_?");
                     }
@@ -14,6 +14,22 @@ public class LoopNestedLoops {
                 System.out.println();
             }
             System.out.println("end");
+
+
+            //if문을 쓰지 않은 경우
+            for (int first = 1; first < 5; first = first + 1) {
+                System.out.println("for " +  first);
+                for (int second=1; second  <= 4; second = second + 1) {
+                    if (second <= first) {
+                        System.out.print("_?");
+                        }
+                        else {
+                            break;
+                        }
+                    }
+                    System.out.println();
+                }
+                System.out.println("end");
         // return 0;
     }
 }
