06412beea0d7a819ae4a16ea00ebdccb2f795e75
aabchyein <abcqaz369@naver.com>
2023-05-24 17:12:21 +0900

    2023-05-24 forth

---
+++ src/B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class B {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        
+        int first = 0;
+        int second = 0;
+        first = myObj.nextInt();
+        second = myObj.nextInt();
+        System.out.println(first + second);
+        // return 0;
+    }
+}
