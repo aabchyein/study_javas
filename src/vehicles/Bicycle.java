3be4288be43b48e32b3f91a37e8fcfc029403a25
aabchyein <abcqaz369@naver.com>
2023-06-19 14:35:01 +0900

    두번째

--- src/vehicles/Bicycle.java
+++ src/vehicles/Bicycle.java
@@ -2,18 +2,18 @@ package vehicles;
 
 import java.util.HashMap;
 
-public class Bicycle { 
+public class Bicycle {  // 아무것도 적혀 있지 않으면 상속받은게 없는 것이 아니라 가장 최상위인 Object가 상속되는 것이다.
     int price ;
     String color ;
     HashMap<String, Object> spec ;
-    public Bicycle() { 
+    public Bicycle() { // 비어있는 생성자
         System.out.println("Bycicle - param empty");
     }
-    public Bicycle(int price, String color) { 
-        this.price = price;   
+    public Bicycle(int price, String color) { // parameter가 들어간 생성자(=변수가 들어가 있는 생성자)(생성자 - 리턴값이 없음, class이름과 동일해야 함)
+        this.price = price;   // 왼쪽은 class 내에 있는 변수인 price, 오른쪽은 parameter로 넘어온 price
         this.color = color;
     }
-    public String toString() { 
+    public String toString() { // method 만듦(리턴값의 data type은 String)
         String str = "price : " + this.price + ", color : "+ this.color;
         return str;
     }
--- src/vehicles/MountainBike.java
+++ src/vehicles/MountainBike.java
@@ -1,18 +1,18 @@
 package vehicles;
 
-public class MountainBike extends Bicycle {  
+public class MountainBike extends Bicycle {  // extends Bicycle (= Bicycle class를 상속 받는다는 의미 - bicycle의 모든 기능을 포함하고 있다고 생각하면 된다.)
     int seatHight;
     public MountainBike() {
 
     }
     public MountainBike(int price, String color, int seatHight) {
-        this.seatHight = seatHight;  
-        super.price = price;   
+        this.seatHight = seatHight;   // class 내부변수인 seatHight는 this를 사용
+        super.price = price;    // 상속받은 class 변수를 그대로 사용할 때는 super를 사용. 이미 상속받아서 이 class 안에도 포함되는 것이므로 this로 써도 동작됨
         super.color = color;
     }
 
-    public String toString() { 
-        String str = "price : " + super.price + ", color : "+ super.color + ", seatHight : " + this.seatHight; 
+    public String toString() {  // Bicycle의 메서드를 그대로 쓸 것이 아니라 재정의(즉 덮어쓰려고 하는 것), 그럼 맨 마지막 정의된 것이 실행됨.
+        String str = "price : " + super.price + ", color : "+ super.color + ", seatHight : " + this.seatHight;  // super라고 안쓰고 this.price, this.color 그대로 써도 무방함.
         return str;
     }
 }
