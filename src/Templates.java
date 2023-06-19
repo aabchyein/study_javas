1fb490d482cca4da4d7769bed132f8ccf0347e2b
aabchyein <abcqaz369@naver.com>
2023-06-19 14:32:45 +0900

    23-06-19 첫번째

---
+++ src/Templates.java
@@ -0,0 +1,13 @@
+public class Templates {public static void main(String[] args) {
+    try {
+        
+    } catch (Exception e) {
+        // TODO: handle exception
+    }
+    System.out.println();
+    
+    //return 0;
+}
+    
+    
+}
---
+++ src/commons/Commons.java
@@ -0,0 +1,10 @@
+package commons;
+
+import java.util.UUID;
+
+public class commons {
+    public String generateUUID() {
+        return UUID.randomUUID().toString();
+    }
+    
+}
